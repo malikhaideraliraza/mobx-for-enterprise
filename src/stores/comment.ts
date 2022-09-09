95498f1ee788ee4a90b73300932ad5da81146adb
Haider Ali <you@example.com>
2022-09-09 13:01:49 +0500

    initail commit

---
+++ src/stores/comment.ts
@@ -0,0 +1,29 @@
+import {
+    action,
+    computed,
+    makeObservable,
+    observable,
+    ObservableMap,
+  } from "mobx";
+  
+  import IComment from "../types/comment";
+  import Comment from "../models/comment";
+  import AppStore from "./app";
+  
+  export default class CommentStore {
+  
+    byId = new observable.map<number, Comment>();
+  
+    constructor(private store: AppStore) {
+      makeObservable(this);
+    }
+  
+    @action load(comments: IComment[]) {
+      comments.forEach((it) => this.byId.set(it.id, new Comment(this.store, it)));
+    }
+  
+    @computed get all() {
+      return Array.from(this.byId.values());
+    }
+  }
+  
---
+++ src/stores/post.ts
@@ -0,0 +1,28 @@
+import {
+    action,
+    computed,
+    makeObservable,
+    observable,
+    ObservableMap,
+  } from "mobx";
+  import Post from "../models/post";
+  import IPost from "../types/post";
+  import AppStore from "./app";
+  
+  export default class PostStore {
+  
+    byId = new observable.map<number, Post>();
+  
+    constructor(private store: AppStore) {
+      makeObservable(this);
+    }
+  
+    @action load(posts: IPost[]) {
+      posts.forEach((it) => this.byId.set(it.id, new Post(this.store, it)));
+    }
+  
+    @computed get all() {
+      return Array.from(this.byId.values());
+    }
+  }
+  
